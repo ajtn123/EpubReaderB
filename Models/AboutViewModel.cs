94b2efcd38b2276ac498ca2b270e958adfd9ebc7
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-08-06 02:05:24 +0800

    remove setters

--- Models/AboutViewModel.cs
+++ Models/AboutViewModel.cs
@@ -7,7 +7,7 @@ public class AboutViewModel(EpubBook? book) : BookViewModelBase(book)
 {
     public new string Title => "About - " + base.Title;
 
-    public string PackageInfo { get; set; } = JsonSerializer.Serialize(book?.Schema.Package, Utils.JsonSerializerOptions);
-    public string Rights { get; set; } = string.Join('\n', book?.Schema.Package.Metadata.Rights.Select(x => x.Rights) ?? []);
-    public string Publishers { get; set; } = string.Join(", ", book?.Schema.Package.Metadata.Publishers.Select(x => x.Publisher) ?? []);
+    public string PackageInfo { get; } = JsonSerializer.Serialize(book?.Schema.Package, Utils.JsonSerializerOptions);
+    public string Rights { get; } = string.Join('\n', book?.Schema.Package.Metadata.Rights.Select(x => x.Rights) ?? []);
+    public string Publishers { get; } = string.Join(", ", book?.Schema.Package.Metadata.Publishers.Select(x => x.Publisher) ?? []);
 }
--- Models/BookViewModelBase.cs
+++ Models/BookViewModelBase.cs
@@ -4,9 +4,9 @@ namespace EpubReaderB.Models;
 
 public class BookViewModelBase(EpubBook? book)
 {
-    public EpubBook? Book { get; set; } = book;
+    public EpubBook? Book { get; } = book;
 
-    public string BookTitle { get; set; } = book?.Title ?? "Untitled";
-    public string Title { get; set; } = book?.Title ?? "Untitled";
-    public string BookCopyright { get; set; } = book != null ? $"{book.Title} © {book.Author}" : "";
+    public string BookTitle { get; } = book?.Title ?? "Untitled";
+    public string Title { get; } = book?.Title ?? "Untitled";
+    public string BookCopyright { get; } = book != null ? $"{book.Title} © {book.Author}" : "";
 }
